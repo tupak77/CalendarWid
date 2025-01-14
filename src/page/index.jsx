a1ba72af04a2b46d3e828ea32688f80c929e1408
tupak77 <81037043+tupak77@users.noreply.github.com>
2025-01-14 11:35:56 +0100

    Add files via upload

---
+++ src/page/index.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container w-[390px] h-[416px] relative mx-auto my-0">
+      <div className="w-[331px] h-[28px] relative z-[88] mt-[30px] mr-0 mb-0 ml-[30px]">
+        <div className="w-[326.155px] h-[24px] relative z-[89] mt-[2px] mr-0 mb-0 ml-[2px]">
+          <span className="flex w-[43.54%] h-full justify-center items-center font-['Inter'] text-[18px] font-bold leading-[24px] text-[#615e82] absolute top-0 left-[28.51%] text-center whitespace-nowrap z-[91]">
+            November 2022
+          </span>
+          <div className="w-[1.56%] h-[43.59%] bg-[url(../assets/images/cfbcb4ba-461d-45a6-8d55-3e1ddd8dab13.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[28.21%] left-0 z-[90]" />
+          <div className="w-[1.56%] h-[43.59%] bg-[url(../assets/images/5aee32e3-710c-4c30-b29a-9663fb937fed.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[28.21%] left-[98.75%] z-[92]" />
+        </div>
+      </div>
+      <div className="w-[330px] h-px bg-[url(../assets/images/45ba29bd-b3e7-46ba-85b6-2e21413c228e.png)] bg-[length:100%_100%] bg-no-repeat relative z-[87] mt-[21px] mr-0 mb-0 ml-[30px]" />
+      <div className="w-[330px] h-[30.093px] relative z-[72] mt-[20px] mr-0 mb-0 ml-[30px]">
+        <div className="w-[9.09%] h-[99.69%] absolute top-0 left-[15.15%] z-[83]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[84]">
+            Tu
+          </span>
+        </div>
+        <div className="w-[9.09%] h-[99.69%] absolute top-[0.31%] left-[90.91%] z-[73]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[74]">
+            Su
+          </span>
+        </div>
+        <div className="w-[9.09%] h-[99.69%] absolute top-[0.31%] left-[75.76%] z-[75]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[76]">
+            Sa
+          </span>
+        </div>
+        <div className="w-[9.09%] h-[99.69%] absolute top-[0.31%] left-[60.61%] z-[77]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[78]">
+            Fr
+          </span>
+        </div>
+        <div className="w-[9.09%] h-[99.69%] absolute top-[0.31%] left-[45.45%] z-[79]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[80]">
+            Th
+          </span>
+        </div>
+        <div className="w-[9.09%] h-[99.69%] absolute top-[0.31%] left-[30.3%] z-[81]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[82]">
+            We
+          </span>
+        </div>
+        <div className="w-[9.09%] h-[99.69%] absolute top-[0.31%] left-0 z-[85]">
+          <span className="flex w-[30px] h-full justify-center items-center font-['Inter'] text-[18px] font-medium leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[86]">
+            Mo
+          </span>
+        </div>
+      </div>
+      <div className="w-full h-full bg-[#fff] rounded-[20px] border-solid border border-[#e5e5ef] absolute top-0 left-0" />
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[84.62%] z-[57]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] absolute top-0 left-0 text-center whitespace-nowrap z-[58]">
+          6
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[71.79%] z-[59]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[60] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[58.97%] z-[61]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[62] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[46.15%] z-[63]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[64] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[33.33%] z-[65]">
+        <div className="w-full h-full bg-[url(../assets/images/397b01b5-f845-4811-8c09-f0369d7c9154.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[66]" />
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#fff] border-none absolute top-0 left-0 whitespace-nowrap z-[67] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[20.51%] z-[68]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[69] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[37.5%] left-[7.69%] z-[70]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#9290a4] absolute top-0 left-0 text-center whitespace-nowrap z-[71]">
+          31
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[84.62%] z-[43]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] absolute top-0 left-0 text-center whitespace-nowrap z-[44]">
+          13
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[71.79%] z-[45]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[46] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[58.97%] z-[47]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[48] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[46.15%] z-[49]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-50 pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[33.33%] z-[51]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[52] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[20.51%] z-[53]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[54] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[49.52%] left-[7.69%] z-[55]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] absolute top-0 left-0 text-center whitespace-nowrap z-[56]">
+          7
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[84.62%] z-[29]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] absolute top-0 left-0 text-center whitespace-nowrap z-30">
+          20
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[71.79%] z-[31]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[32] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[58.97%] z-[33]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[34] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[46.15%] z-[35]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[36] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[33.33%] z-[37]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[38] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[20.51%] z-[39]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-40 pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[61.54%] left-[7.69%] z-[41]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] absolute top-0 left-0 text-center whitespace-nowrap z-[42]">
+          14
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[84.62%] z-[15]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[16] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[71.79%] z-[17]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[18] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[58.97%] z-[19]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-20 pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[46.15%] z-[21]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[22] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[33.33%] z-[23]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[24] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[20.51%] z-[25]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[26] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[73.56%] left-[7.69%] z-[27]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] absolute top-0 left-0 text-center whitespace-nowrap z-[28]">
+          21
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[84.13%] left-[84.62%] z-[1]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#fff] absolute top-0 left-0 text-center whitespace-nowrap z-[2]">
+          3
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[84.13%] left-[71.79%] z-[3]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#fff] absolute top-0 left-0 text-center whitespace-nowrap z-[4]">
+          2
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[84.13%] left-[58.97%] z-[5]">
+        <span className="flex w-[30px] h-[30px] justify-center items-center font-['Inter'] text-[18px] font-normal leading-[20px] text-[#fff] absolute top-0 left-0 text-center whitespace-nowrap z-[6]">
+          1
+        </span>
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[85.58%] left-[46.15%] z-[7]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[8] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[85.58%] left-[33.33%] z-[9]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-10 pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[85.58%] left-[20.51%] z-[11]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[12] pointer" />
+      </div>
+      <div className="w-[7.69%] h-[7.15%] absolute top-[85.58%] left-[7.69%] z-[13]">
+        <button className="w-[30px] h-[30px] font-['Inter'] text-[18px] font-normal leading-[20px] text-[#1d1c2a] border-none absolute top-0 left-0 whitespace-nowrap z-[14] pointer" />
+      </div>
+    </div>
+  );
+}
